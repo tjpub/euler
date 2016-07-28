809bdf5c18a3fb6244659bab5fe802a041e85fa8
TJ <tj@si.hu>
2016-07-28 12:32:44 +0200

    done

--- 1.go
+++ 1.go
@@ -1,24 +1,26 @@
-# Compiled Object files, Static and Dynamic libs (Shared Objects)
-*.o
-*.a
-*.so
+package main
 
-# Folders
-_obj
-_test
+/*
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 
-# Architecture specific extensions/prefixes
-*.[568vq]
-[568vq].out
+Find the sum of all the multiples of 3 or 5 below 1000.
+*/
 
-*.cgo1.go
-*.cgo2.c
-_cgo_defun.c
-_cgo_gotypes.go
-_cgo_export.*
+import "fmt"
 
-_testmain.go
+const MAX int = 1000
 
-*.exe
-*.test
-*.prof
+func main() {
+	fmt.Println("doing eiler 1")
+
+	var i int = 0
+
+	for index := 0; index < MAX; index++ {
+		if index%3 == 0 || index%5 == 0 {
+			i += index
+		}
+	}
+
+	fmt.Printf("result: %d\n", i)
+	fmt.Println("done")
+}
